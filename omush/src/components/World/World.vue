9429f8d267f5344e167265aee65d852e6a6dd613
Michael Smith <Michael.Smith@vml.com>
2016-09-16 13:37:07 -0500

    Make the world open a websocket connect and handle error, close, open, message and sending messages.

--- omush/src/components/World/World.vue
+++ omush/src/components/World/World.vue
@@ -10,34 +10,87 @@ import DisplayMessageFactory from './DisplayMessageFactory'
 import OutputWindow from './OutputWindow'
 import InputWindow from './InputWindow'
 
+import {WSocket} from '../WebSocket/WSocket'
+
 export default {
   components: {
     OutputWindow,
     InputWindow
   },
+
   props: [
     'host',
     'port'
   ],
+
   data () {
     return {
+      wsock: null
     }
   },
+
   computed: {
   },
+
   methods: {
     displayMessage: function (msg) {
-      console.log(typeof msg)
       this.$broadcast('displayMessage', msg)
+    },
+
+    connect: function () {
+      let dMsg = DisplayMessageFactory.systemMessage('Trying to connect...')
+      this.displayMessage(dMsg)
+      let sock = this.getSocket()
+      sock.connect()
+    },
+
+    getSocket: function () {
+      if (this.wsock === null) {
+        this.wsock = new WSocket(this.host, this.port)
+        this.wsock.registerListener(this)
+      }
+
+      return this.wsock
+    },
+
+    send: function (message) {
+      let sock = this.getSocket()
+      sock.send(message)
+    },
+
+    onSocketMessage: function (event) {
+      switch (event.event) {
+        case 'error':
+          this.displayMessage(
+            DisplayMessageFactory.systemMessage('Unable to connect to host and port.'))
+          break
+        case 'close':
+          this.displayMessage(
+            DisplayMessageFactory.systemMessage('Connection Closed.'))
+          break
+        case 'open':
+          this.displayMessage(
+            DisplayMessageFactory.systemMessage('Connection Open.'))
+          break
+        case 'message':
+          this.displayMessage(
+            DisplayMessageFactory.message(event.message.data))
+          break
+        default:
+          console.log('Default message: ' + event.message)
+          break
+      }
     }
   },
+
   ready () {
-    let dMsg = DisplayMessageFactory.systemMessage('Trying to connect...')
-    this.displayMessage(dMsg)
+    this.connect()
   },
+
   events: {
     'sendInput': function (msg) {
       this.displayMessage(DisplayMessageFactory.localEchoMessage(msg))
+      this.send(msg)
     }
   }
 }
